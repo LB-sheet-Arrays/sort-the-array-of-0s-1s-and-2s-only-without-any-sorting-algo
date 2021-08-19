6710ade48a9d94fd28db0df785a4ee1600d098f6
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-08-19 11:33:02 +0530

    Update Q4 sort 0 1 and 2 without sorting.c

--- Q4 sort 0 1 and 2 without sorting.c
+++ Q4 sort 0 1 and 2 without sorting.c
@@ -61,42 +61,4 @@ void main()
 	printf("\n The sorted array is \n\n");
 	printArray(a,n);
 */
-
-//Utkarsh's approach --> Directly print the array.  
-int count0=0,count1=0,count2=0;  //Ye bhi 3 variables ki hi space kha raha hai.
-for(i=0;i<n;i++)
-{
-	if(a[i]==0)
-	{
-		count0++;
-	}
-	if(a[i]==1)
-	{
-		count1++;
-	}
-	if(a[i]==2)
-	{
-		count2++;
-	}
-}
-
-if((count0+count1+count2)==n)
-{
-	printf("\n { ");
-	//Modify and print the input array itself to preserve the space.
-	for(i=0;i<count0;i++)
-	{
-		printf(" 0 ");
-	}
-	for(i=count0;i<(count0+count1);i++)
-	{
-		printf(" 1 ");
-	}
-	for(i=count1;i<(count1+count2);i++)
-	{
-		printf(" 2 ");
-	}
-	printf(" } ");
-}
-
 }
